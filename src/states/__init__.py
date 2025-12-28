659d8ad59b27489a52c5fc5c34d842d1bbfde415
Neritoou <agdsds245@gmail.com>
2025-12-28 18:16:03 -0400

    refactor: restructure the access to states

--- src/states/__init__.py
+++ src/states/__init__.py
@@ -1,9 +1,10 @@
 from .game_state import GameState
 from .play_state import PlayState
 from .state_manager import StateManager
+from .state_id import StateID
 
 __all__ = [
-    "GameState",
     "PlayState",
-    "StateManager"
+    "StateManager",
+    "StateID"
 ]

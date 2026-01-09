b3a0f2e3130b42c8eba23b3cb81da7aff4d172e9
odetax <odettsayegh@gmail.com>
2026-01-09 01:25:39 -0400

    refactor: optimize font resource mapping and loading logic

--- src/resources/loaders/fonts.py
+++ src/resources/loaders/fonts.py
@@ -3,5 +3,6 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
         from ..resource_manager import ResourceManager
 
-def _load_fonts(rm: "ResourceManager"):
-        pass
+def _load_fonts(rm: "ResourceManager") -> None:
+        different_sizes = {48, 90}
+        rm.load_font("Estandar", "assets/fonts/04B03.ttf", different_sizes)

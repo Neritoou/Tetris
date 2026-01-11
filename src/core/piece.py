4c7019af4fa6bf077a1c71cdd602c3d53e9b6b56
Neritoou <agdsds245@gmail.com>
2026-01-11 05:01:11 -0400

    feat: add active atribute and methods to find out the condition of the piece

--- src/core/piece.py
+++ src/core/piece.py
@@ -1,7 +1,7 @@
 import pygame
 import numpy as np
 from typing import List, Tuple
-from ..constants import BLOCK_W, BLOCK_H
+from ..constants import BLOCK_W, BLOCK_H, WALL_KICKS
 
 from .types import PieceData
 
@@ -26,8 +26,8 @@ class Piece:
         self._data = data
         self.row = row
         self.col = col
-        self.rot = rot
-    
+        self.rot = rot 
+        self.active = True
     
     @property
     def type(self) -> int:
@@ -37,6 +37,18 @@ class Piece:
     def matrix(self) -> np.ndarray:
         """Devuelve la matriz correspondiente a la rotación actual de la pieza."""
         return self._data["matrices"][self.rot]
+    
+    def is_locked(self) -> bool:
+        """Retorna verdadero si la pieza ya está bloqueada en el tablero"""
+        return not self.active
+    
+    def set_locked(self) -> None:
+        """Cambia el estado de la pieza a bloqueada"""
+        self.active = False
+
+    def set_actived(self) -> None:
+        """Cambia el estado de la pieza a activada"""
+        self.active = True
     
     def draw_normal(self, surface: pygame.Surface, position_in_board: Tuple[int, int]) -> None:
         """Dibuja la pieza normal usando su surface de bloque normal."""
@@ -69,10 +81,14 @@ class Piece:
         # Se le suma una rotación a la actual y se aplica el % para controlar el índice de rotación
         self.rot = (self.rot + direction) % total
 
-    def center(self, board_cols: int):
-        """Centra las piezas en la board."""
+    def center(self, board_cols: int, spawn_offset: int = 0):
+        """
+        Centra horizontalmente la pieza en el tablero y aplica un spawn_offset
+        vertical para generarla fuera de la pantalla si spawn_offset < 0.
+        """
         matrix_width = self.matrix.shape[1]
         self.col = (board_cols - matrix_width) // 2
+        self.row = spawn_offset
     
     def get_cells(self, row: int | None = None, col: int | None = None) -> List[Tuple[int, int]]:
         """
@@ -93,6 +109,34 @@ class Piece:
             if block: 
                 cells.append((baserow + r, basecol + c))
         return cells
+    
+    def get_wall_kicks(self, old_rot: int, new_rot: int):
+        """
+        Obtiene los Wall Kicks necesarios para una rotación de pieza, basándose en el tipo de pieza 
+        y las rotaciones previas y actuales.
+
+        Los Wall Kicks son desplazamientos que se aplican a una pieza cuando no puede rotar debido 
+        a una colisión, pero puede moverse 
+        ligeramente para que la rotación sea válida.
+
+        Args:
+            old_rot (int): La rotación previa de la pieza (en grados: 0, 90, 180, 270).
+            new_rot (int): La nueva rotación de la pieza (en grados: 0, 90, 180, 270).
+            piece_type (str): El tipo de pieza que se está rotando (por ejemplo, "I", "T", "J", "L", "S", "Z", "O").
+
+        Returns:
+            List[Tuple[int, int]]: Una lista de desplazamientos (dx, dy) que representan los posibles wall kicks para realizar la rotación.
+            Si no se encuentran desplazamientos, devuelve [(0, 0)], lo que indica que no hay necesidad de aplicar un wall kick.
+        """
+        if self.name == "I":
+            table = WALL_KICKS["I"]
+        elif self.name != "O":
+            table = WALL_KICKS["OTHERS"]
+        else:
+            return [(0, 0)]
+
+        return table.get((old_rot % 4, new_rot % 4), [(0, 0)])
+
     
     # --- HELPERS ---
     def _draw_blocks(self, surface: pygame.Surface, position_in_board: Tuple[int, int], block_surface: pygame.Surface) -> None:

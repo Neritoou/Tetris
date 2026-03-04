e7ccb53d6a853941082b9eca364ce9e038016b41
Neritoou <agdsds245@gmail.com>
2026-03-04 15:03:16 -0400

    refactor: extract piece mechanics into dedicated PieceMechanics class

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,6 +1,22 @@
-from src.core.types import PieceData, PieceSurfaces, BlockSurfaces, PiecesPreviewType, BoardType, PieceDataType
+from src.core.types import PieceData, BlockSurfaces, PieceSurfaces, PiecesPreviewType, BoardType, PieceDataType
 from src.core.board import Board
 from src.core.piece import Piece
 from src.core.piece_bag import PieceBag
+from src.core.piece_mechanics import PieceMechanics
 from src.core.score import Score
 from src.core.gameboard_controller import GameBoardController
+
+__all__ = [
+    "PieceData",
+    "BlockSurfaces",
+    "PieceSurfaces",
+    "PiecesPreviewType",
+    "BoardType",
+    "PieceDataType",
+    "Board",
+    "Piece",
+    "PieceBag",
+    "PieceMechanics",
+    "Score",
+    "GameBoardController",
+]

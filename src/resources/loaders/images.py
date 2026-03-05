1b5dcc2d4902f305b32e8207da8135a13d4767c6
odetax <odettsayegh@gmail.com>
2026-03-04 22:45:50 -0400

    feat: add UI images for buttons, arrows and new board layout

--- src/resources/loaders/images.py
+++ src/resources/loaders/images.py
@@ -6,4 +6,14 @@ if TYPE_CHECKING:
 
 def _load_images(rm: "ResourceManager") -> None:
         # BOARD BASE
-        rm.load_image("Board",str(get_asset("images","grid.png")))
+        rm.load_image("Title", str(get_asset("images", "tetris_title.png")))
+        rm.load_image("Background", str(get_asset("images", "background.png")))
+
+        rm.load_image("Board", str(get_asset("images", "board.png")))
+        rm.load_image("MenuArrows", str(get_asset("images", "menu_arrows.png")))
+
+        rm.load_image("RulesetNormalBtn", str(get_asset("images", "ruleset_normal_btn.png")))
+        rm.load_image("RulesetSelBtn", str(get_asset("images", "ruleset_sel_btn.png")))
+
+        rm.load_image("ControlsNormalBtn", str(get_asset("images", "controls_normal_btn.png")))
+        rm.load_image("ControlsSelBtn", str(get_asset("images", "controls_sel_btn.png")))
